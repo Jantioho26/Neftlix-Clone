9c510c971ea39cc463ba76f1628b5ec1e182fbfe
Joy Antioho <j.wildfong@yahoo.com>
2026-08-12 16:56:16 -0400

    Netflix-Clone

--- Desktop/Netflix Clone/netflix-clone/src/App.js
+++ Desktop/Netflix Clone/netflix-clone/src/App.js
@@ -7,10 +7,19 @@ import requests from './requests';
 
 function App() {
   return (
-    <div className="App">
-    <h1> Hey Programmer, let's build Netflix Clone Frontend!</h1>
+    <div className="app">
+    <h1>
+      Let's build a Netflix CLone!
+      </h1>
+
     <Row title="NETFLIX ORIGINALS" fetchURL={requests.fetchNetflixOriginals} />
     <Row title="Trending Now" fetchURL={requests.fetchTrending} />
+    <Row title="Top Rated" fetchURL={requests.fetchTopRated} />
+    <Row title="Action Movies" fetchURL={requests.fetchActionMovies} />
+    <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies} />
+    <Row title="Horror Movies" fetchURL={requests.fetchHorrorMovies} />
+    <Row title="Romance Movies" fetchURL={requests.fetchRomanceMovies} />
+    <Row title="Documentaries" fetchURL={requests.fetchDocumentaries} />
     </div>
   );
 } 

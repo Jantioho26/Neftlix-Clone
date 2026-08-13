a0e08fcc391782170958950141602e8646d3354c
Joy Antioho <j.wildfong@yahoo.com>
2026-08-13 13:09:52 -0400

    Netflix-Clone

--- Desktop/Netflix Clone/netflix-clone/src/App.js
+++ Desktop/Netflix Clone/netflix-clone/src/App.js
@@ -12,11 +12,16 @@ function App() {
     <Banner />  
  <Row 
       title="NETFLIX ORIGINALS"
-       fetchUrl={requests.fetchNetflixOriginals}
+       fetchURL={requests.fetchNetflixOriginals}
        isLargeRow={true} 
        />
-      <Row title="Trending Now"  fetchUrl={requests.fetchTrending}/>
-      <Row title="Top Rated"  fetchUrl={requests.fetchTopRated}/>
+      <Row title="Trending Now"  fetchURL={requests.fetchTrending}/>
+      <Row title="Top Rated"  fetchURL={requests.fetchTopRated}/>
+      <Row title="Action Movies"  fetchURL={requests.fetchActionMovies}/>
+      <Row title="Comedy Movies"  fetchURL={requests.fetchComedyMovies}/>
+      <Row title="Horror Movies"  fetchURL={requests.fetchHorrorMovies}/>
+      <Row title="Romance Movies"  fetchURL={requests.fetchRomanceMovies}/>
+      <Row title="Documentaries"  fetchURL={requests.fetchDocumentaries}/>
 
     </div>
   );
--- Desktop/Netflix Clone/netflix-clone/src/Row.js
+++ Desktop/Netflix Clone/netflix-clone/src/Row.js
@@ -3,7 +3,7 @@ import axios from './axios';
 import requests from './requests';
 import './Row.css';
 
-const base_url = "https://image.tmdb.org/t/p/original";
+const base_URL = "https://image.tmdb.org/t/p/original";
 
 const Row = ({ title, fetchURL, isLargeRow })  => {
     const [movies, setMovies] = useState([]);
@@ -11,7 +11,7 @@ const Row = ({ title, fetchURL, isLargeRow })  => {
 useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchURL);
-      setMovies(request.data.results);
+      setMovies(request.data.results || []);
       return requests; 
     }
     fetchData();
@@ -29,7 +29,7 @@ console.table(movies)
             <img 
             key={movie.id}
             className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-            src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
+            src={`${base_URL}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
             alt={movie.name} 
             />
         ))}
